f44b6f1ec9943a4c444a4b0d19768d125eedc8c5
lmiguela <40177138+lmiguela@users.noreply.github.com>
2018-10-18 07:50:39 -0500

    Create funciones.h

---
+++ funciones.h
@@ -0,0 +1,12 @@
+#ifndef INC_2_FUNCIONES_H
+#define INC_2_FUNCIONES_H
+
+#include <string>
+using namespace std;
+
+
+void MenuJuego(int &n,string r1[10][3], int &contador);
+void JugarCompletarRefranes(int &n,string r1[10][3],int &contador);
+void MostrarResultados(int contador);
+//void mostrarSignificado(int ,significado);
+#endif

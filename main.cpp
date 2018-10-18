5e6eac26f982d1289018adaf3bf46b0e1c64ad07
lmiguela <40177138+lmiguela@users.noreply.github.com>
2018-10-18 07:55:09 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -10,13 +10,13 @@ int main() {
     int n,contador=0;
     string r1[9][3]={{"A palabras necias", "oidos sordos","uno"} ,
                       {"Ojos que no ven", "corazón que no siente","dos"},
-                      {"en boca cerrada", "no entran moscas","tres"},
-                      {"no hay mal que por bien","no venga","cuatro"},
-                      {"no dejes para ma;ana","lo que puedes hacer hoy","cinco"},
-                      {"perro que ladra,","no muerde","sesie"},
-                      {"al que madruga","dios le ayuda","siete"},
-                      {"aunque la mona se vista de seda,","mona se queda","ocho"},
-                      {"a caballo regalado,","no se le miran los dientes","nuevo"}
+                      {"En boca cerrada", "no entran moscas","tres"},
+                      {"No hay mal que por bien","no venga","cuatro"},
+                      {"No dejes para ma;ana","lo que puedes hacer hoy","cinco"},
+                      {"Perro que ladra","no muerde","seis"},
+                      {"Al que madruga","dios le ayuda","siete"},
+                      {"Aunque la mona se vista de seda,","mona se queda","ocho"},
+                      {"A caballo regalado","no se le miran los dientes","nuevo"}
     };
 
     vector<string> significado {"no hay que hacer caso del que habla sin razon","no se sufre por lo que no se sabe","es a veces muy util callar","a veces,algo negativo trae consecuencia positivas","hay que tratar de vivir el dia presente realizando los anhelos o deberes","los que hablan mucho,suelen hacer poci","muchas veces, el exito depende de la rapidez","no importa con que vista una persona, ella siempre sera la misma","sera el tiempo quien diga quien tiene la razon quien se equivoca"};
